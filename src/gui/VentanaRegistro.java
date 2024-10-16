ab6499526d69321dbddf3767c3eb3554f5e09e26
Paula de Velasco Zaldibar <paulade.velasco@opendeusto.es>
2024-10-16 12:42:46 +0200

    Sigo con el desarrollo de la pagina registro

--- src/gui/VentanaRegistro.java
+++ src/gui/VentanaRegistro.java
@@ -4,47 +4,81 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class VentanaRegistro extends JFrame{
 	//Declaramos los componentes boton
 	private JButton btnInicioSesion, btnCierreSesion;
 	//Declaramos los paneles
-	private JPanel pCentro, pNorte, PEste, POeste, PSur;
+	private JPanel pCentro, pNorte, pEste, pOeste, pSur;
+	//Declaramos los componentes etiqueta
+	private JLabel lblTitulo, lblNombreUsuario, lblContraseniaUsuario;
+	//Declaramos los componente cuadro de texto
+	private JTextField txtNombreUsuario;
+	private JPasswordField txtContraseniaUsuario;
 	
 	public VentanaRegistro() {
 		super();
 		setBounds(300,200,600,400);
+		
+		//Editar el nombre de la ventana
+		setTitle("SkyMovie");
+		//Ponerle un icono personalizado a la ventana (esquina superior izquierda)
+		ImageIcon imagen = new ImageIcon("img/iconoSkyMovie.png");
+		setIconImage(imagen.getImage());
 		
 		//Instanciamos los paneles
 		pCentro = new JPanel();
 		//Modificamos el Layout del panel centro
 		pCentro.setLayout(new GridLayout(2, 2));
 		pNorte = new JPanel();
-		PSur = new JPanel();
-		PEste = new JPanel();
-		POeste = new JPanel();
+		pSur = new JPanel();
+		pEste = new JPanel();
+		pOeste = new JPanel();
 		
 		//Añadimos los paneles al panel principal de la ventana
 		getContentPane().add(pNorte, BorderLayout.NORTH);
-		getContentPane().add(PEste, BorderLayout.EAST);
-		getContentPane().add(POeste, BorderLayout.WEST);
-		getContentPane().add(PSur, BorderLayout.SOUTH);
-		//getContentPane().add(PCentro, BorderLayout.CENTER);
+		getContentPane().add(pEste, BorderLayout.EAST);
+		getContentPane().add(pOeste, BorderLayout.WEST);
+		getContentPane().add(pSur, BorderLayout.SOUTH);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
 
 		
 		//Instanciamos los componentes botón
 		btnInicioSesion = new JButton("Iniciar Sesion");
 		btnCierreSesion = new JButton("Cerrar Sesion");
+		
 		//Cambiamos el tipo de letra en los botones
 		btnInicioSesion.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
 		btnCierreSesion.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
-		//Añadimos el boton al panel sur de la ventana
-		PSur.add(btnInicioSesion);
-		PSur.add(btnCierreSesion);
 	
+		
+		lblTitulo = new JLabel("ONGI ETORRI!!");
+		lblNombreUsuario = new JLabel("Introduce tu nombre de usuario: ");
+		lblContraseniaUsuario = new JLabel("Introduce tu contraseña: ");
+		
+		txtNombreUsuario = new JTextField(20);
+		txtContraseniaUsuario = new JPasswordField(20);
+		
+		//Añadimos el boton al panel sur de la ventana
+		pSur.add(btnInicioSesion);
+		pSur.add(btnCierreSesion);
+		
+		pNorte.add(lblTitulo);
+		pCentro.add(lblNombreUsuario);
+		pCentro.add(txtNombreUsuario);
+		pCentro.add(lblContraseniaUsuario);
+		pCentro.add(txtContraseniaUsuario);
+		
 		setVisible(true);
+	}
+	public static void main(String[] args) {
+		VentanaRegistro v = new VentanaRegistro();
 	}
 }
