1f267df2625ce53c75c85b0983bf14fe91370ff1
Paula de Velasco Zaldibar <paulade.velasco@opendeusto.es>
2024-10-24 19:02:01 +0200

    ventanaIncial arreglos

--- src/gui/VentanaInicial.java
+++ src/gui/VentanaInicial.java
@@ -15,20 +15,54 @@ public class VentanaInicial extends JFrame {
 
     // Lista de rutas de las imágenes de las películas
     private List<String> rutasImagenes = List.of(
-        "/resources/img/portadas/imagen1.jpg",
-        "/resources/img/portadas/imagen2.jpg",
-        "/resources/img/portadas/imagen3.jpg",
-        "/resources/img/portadas/imagen4.jpg",
-        "/resources/img/portadas/imagen5.jpg",
-        "/resources/img/portadas/imagen6.jpg",
-        "/resources/img/portadas/imagen7.jpg"
+        "/resources/img/portadas/12 Años de Esclavitud.jpg",
+        "/resources/img/portadas/Alien El Octavo Pasajero.jpg",
+        "/resources/img/portadas/Barbie.jpg",
+        "/resources/img/portadas/Blade Runner.jpg",
+        "/resources/img/portadas/Casablanca.jpg",
+        "/resources/img/portadas/Dune Parte uno.jpg",
+        "/resources/img/portadas/El Caballero Oscuro.jpg",
+        "/resources/img/portadas/El Club de la Pelea.jpg",
+        "/resources/img/portadas/El Gran Escape.jpg",
+        "/resources/img/portadas/El Padrino.jpg",
+        "/resources/img/portadas/El renacido.jpg",
+        "/resources/img/portadas/El Rey Leon.jpg",
+        "/resources/img/portadas/El Señor de los Anillos La Comunidad del Anillo.jpg",
+        "/resources/img/portadas/Encanto.jpeg",
+        "/resources/img/portadas/Forrest Gump.jpg",
+        "/resources/img/portadas/Frozen II.jpeg",
+        "/resources/img/portadas/Gladiador.jpg",
+        "/resources/img/portadas/Gran Torino.jpg",
+        "/resources/img/portadas/Indiana Jones En busca del arca perdida.png",
+        "/resources/img/portadas/Jurassic Park.jpg",
+        "/resources/img/portadas/La Ballena.jpeg",
+        "/resources/img/portadas/La Guerra de las Galaxias.jpg",
+        "/resources/img/portadas/Los Infiltrados.jpg",
+        "/resources/img/portadas/Luca.jpeg",
+        "/resources/img/portadas/Matrix.jpg",
+        "/resources/img/portadas/Minions El Origen de Gru.jpg",
+        "/resources/img/portadas/MisionImposible Sentencia Mortal - Parte uno.jpg",
+        "/resources/img/portadas/Oppenheimer.jpg",
+        "/resources/img/portadas/Slumdog Millionaire.jpg",
+        "/resources/img/portadas/Soul.jpg",
+        "/resources/img/portadas/Spider Man Cruzando el multiverso.jpg",
+        "/resources/img/portadas/Tiempos Modernos.jpg",
+        "/resources/img/portadas/Titanic.jpg",
+        "/resources/img/portadas/Toy Story 4.jpg",
+        "/resources/img/portadas/Volver al Futuro.jpg"
     );
 
     // Lista de títulos de las películas
     private List<String> titulosPeliculas = List.of(
-        "Película 1", "Película 2", "Película 3", 
-        "Película 4", "Película 5", "Película 6", 
-        "Película 7"
+        "12 Años de Esclavitud", "Alien El Octavo Pasajero", "Barbie", 
+        "Blade Runner", "Casablanca", "Dune Parte uno", "El Caballero Oscuro",
+        "El Club de la Pelea", "El Gran Escape", "El Padrino", "El renacido", "El Rey Leon",
+        "El Señor de los Anillos La Comunidad del Anillo", "Encanto", "Forrest Gump", 
+        "Frozen II", "Gladiador", "Gran Torino", "Indiana Jones En busca del arca perdida", 
+        "Jurassic Park", "La Ballena", "La Guerra de las Galaxias", "Los Infiltrados", 
+        "Luca", "Matrix", "Minions El Origen de Gru", "MisionImposible Sentencia Mortal - Parte uno",
+        "Oppenheimer", "Slumdog Millionaire", "Soul", "Spider Man Cruzando el multiverso", "Tiempos Modernos",
+        "Titanic", "Toy Story 4", "Volver al Futuro"
     );
 
     public VentanaInicial() {
