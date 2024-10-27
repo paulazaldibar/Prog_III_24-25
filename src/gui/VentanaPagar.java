48ad43d63d8a56755d84006fcfae917348988882
Lara Poncela <lara.p@opendeusto.es>
2024-10-27 11:30:36 +0100

    VentanaPagar acabada

--- src/gui/VentanaPagar.java
+++ src/gui/VentanaPagar.java
@@ -13,86 +13,87 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class VentanaPagar extends JFrame{
-	private JButton btnPagar; 
-	private JPanel pCentro, pNorte, pSur; 
-	private JLabel lblTitulo, lblNombre, lblTarjeta, lblFecha, lblCVV;
-	private JTextField txtNombre, txtTarjeta, txtFecha, txtCVV; 
-	private JCheckBox checkTerminos;
-	private JTextArea txtCondiciones; 
-	
-	public VentanaPagar() {
-		super();
-		setBounds(300,200,600,400);
-		
-		setTitle("SkyMovie");
-		
-		ImageIcon imagen = new ImageIcon("resources/img/iconoSkyMovie.png");
-		setIconImage(imagen.getImage());
-		
-		pCentro = new JPanel();
-		pNorte = new JPanel(); 
-		pSur = new JPanel(); 
-		
-		
-		getContentPane().add(pCentro, BorderLayout.CENTER);
-		getContentPane().add(pNorte, BorderLayout.NORTH);
-		getContentPane().add(pSur, BorderLayout.SOUTH);
-		
-		pCentro.setLayout(new GridLayout(6, 2, 10, 10)); // 6 filas, 2 columnas
-		pCentro.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // para dejar espacio en los bordes
+public class VentanaPagar extends JFrame {
+    private JButton btnPagar;
+    private JPanel pCentro, pNorte, pSur, pCentroDatos;
+    private JLabel lblTitulo, lblNombre, lblTarjeta, lblFecha, lblCVV;
+    private JTextField txtNombre, txtTarjeta, txtFecha, txtCVV;
+    private JCheckBox checkTerminos;
+    private JTextArea txtCondiciones;
 
-		btnPagar = new JButton("PAGAR");
-		
-		btnPagar.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
-		
-		lblTitulo = new JLabel("Introduzca sus datos para realizar el pago:");
-		lblNombre = new JLabel("Nombre: ");
-		lblTarjeta = new JLabel("Número de tarjeta: ");
-		lblFecha = new JLabel("Fecha de caducidad: ");
-		lblCVV = new JLabel("CVV: "); 
-		
-		txtNombre = new JTextField(50);
-		txtTarjeta = new JTextField(20);
-		txtFecha = new JTextField("MM/AA", 5);
-		txtCVV = new JTextField(3);
-		
-		checkTerminos = new JCheckBox("Aceptar términos y condiciones");
-		
-		txtCondiciones = new JTextArea("Al proceder con la compra, usted acepta los términos y condiciones," 
-									+ "así como el uso de sus datos personales para fines de gestión de compra "
-									+ "y entrega de servicios. La información será tratada de acuerdo con la "
-									+ "legislación vigente en materia de protección de datos y nuestra política"
-									+ " de privacidad, la cual puede consultar en nuestra página web.");
-		txtCondiciones.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 10)); 
-		
-		//txtCondiciones.setLineWrap(true);
-       // txtCondiciones.setWrapStyleWord(true);
-        txtCondiciones.setEditable(false); // Solo lectura
-        txtCondiciones.setBackground(getBackground()); // Sin fondo visible
-		 
-		
-		
-		pSur.add(btnPagar);
-		pNorte.add(lblTitulo);
-		pCentro.add(lblNombre);
-		pCentro.add(txtNombre);
-		pCentro.add(lblTarjeta);
-		pCentro.add(txtTarjeta);
-		pCentro.add(lblFecha);
-		pCentro.add(txtFecha);
-		pCentro.add(lblCVV); 
-		pCentro.add(txtCVV);
-		pCentro.add(checkTerminos);
-		pCentro.add(txtCondiciones);
-		
-		setVisible(true);
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		VentanaPagar vPagar = new VentanaPagar();
-	}
-	
+    public VentanaPagar() {
+        super();
+        setBounds(300, 200, 600, 400);
+        
+        setTitle("SkyMovie");
+
+        ImageIcon imagen = new ImageIcon("resources/img/iconoSkyMovie.png");
+        setIconImage(imagen.getImage());
+
+        pCentro = new JPanel(new BorderLayout(10, 10));
+        pNorte = new JPanel();
+        pSur = new JPanel();
+        pCentroDatos = new JPanel(new GridLayout(5, 2, 10, 10));
+
+        getContentPane().add(pCentro, BorderLayout.CENTER);
+        getContentPane().add(pNorte, BorderLayout.NORTH);
+        getContentPane().add(pSur, BorderLayout.SOUTH);
+
+        pCentro.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        btnPagar = new JButton("PAGAR");
+        btnPagar.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+
+        lblTitulo = new JLabel("Introduzca sus datos para realizar el pago:");
+        lblNombre = new JLabel("Nombre: ");
+        lblTarjeta = new JLabel("Número de tarjeta: ");
+        lblFecha = new JLabel("Fecha de caducidad: ");
+        lblCVV = new JLabel("CVV: ");
+
+        txtNombre = new JTextField(50);
+        txtTarjeta = new JTextField(20);
+        txtFecha = new JTextField("MM/AA", 5);
+        txtCVV = new JTextField(3);
+
+        checkTerminos = new JCheckBox("Aceptar términos y condiciones");
+
+        txtCondiciones = new JTextArea("Al proceder con la compra, usted acepta los términos y condiciones,"
+                + " así como el uso de sus datos personales para fines de gestión de compra "
+                + "y entrega de servicios. La información será tratada de acuerdo con la "
+                + "legislación vigente en materia de protección de datos y nuestra política"
+                + " de privacidad, la cual puede consultar en nuestra página web.");
+        txtCondiciones.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 10));
+        txtCondiciones.setLineWrap(true); // Permite hacer saltos de línea
+        txtCondiciones.setWrapStyleWord(true); // En los saltos de línea separa las palabras completas, no las separa a la mitad
+        txtCondiciones.setEditable(false); // No permite modificar el texto
+        txtCondiciones.setBackground(getBackground()); // Mantiene el color de fondo puesto por defecto
+ 
+        // Añadimos el título en el panel norte
+        pNorte.add(lblTitulo);
+        
+        // Añadimos los componentes al panel de datos
+        pCentroDatos.add(lblNombre);
+        pCentroDatos.add(txtNombre);
+        pCentroDatos.add(lblTarjeta);
+        pCentroDatos.add(txtTarjeta);
+        pCentroDatos.add(lblFecha);
+        pCentroDatos.add(txtFecha);
+        pCentroDatos.add(lblCVV);
+        pCentroDatos.add(txtCVV);
+        pCentroDatos.add(checkTerminos);
+
+        // Añadimos los datos y el área de texto al panel central
+        pCentro.add(pCentroDatos, BorderLayout.NORTH);
+        pCentro.add(txtCondiciones, BorderLayout.CENTER);
+
+        // Añadimos el botón al panel sur dejando un pequeño borde
+        pSur.add(btnPagar);
+        pSur.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+    	VentanaPagar vPagar = new VentanaPagar();
+    }
 }
