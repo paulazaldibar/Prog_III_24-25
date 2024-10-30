381f08509137eb74cbb8bee6e6ea03a5b4681e9c
Paula de Velasco Zaldibar <paulade.velasco@opendeusto.es>
2024-10-30 16:39:25 +0100

    Errores dentro de ventana inicial sin resolver

--- src/gui/VentanaInicial.java
+++ src/gui/VentanaInicial.java
@@ -16,41 +16,41 @@ import javax.swing.JTabbedPane;
 public class VentanaInicial extends JFrame {
 
     private List<String> rutasImagenes = List.of(
-        "/resources/img/portadas/12 Años de Esclavitud.jpg",
-        "/resources/img/portadas/Alien El Octavo Pasajero.jpg",
-        "/resources/img/portadas/Barbie.jpg",
-        "/resources/img/portadas/Blade Runner.jpg",
-        "/resources/img/portadas/Casablanca.jpg",
-        "/resources/img/portadas/Dune Parte uno.jpg",
-        "/resources/img/portadas/El Caballero Oscuro.jpg",
-        "/resources/img/portadas/El Club de la Pelea.jpg",
-        "/resources/img/portadas/El Gran Escape.jpg",
-        "/resources/img/portadas/El Padrino.jpg",
-        "/resources/img/portadas/El renacido.jpg",
-        "/resources/img/portadas/El Rey Leon.jpg",
-        "/resources/img/portadas/El Señor de los Anillos La Comunidad del Anillo.jpg",
-        "/resources/img/portadas/Encanto.jpeg",
-        "/resources/img/portadas/Forrest Gump.jpg",
-        "/resources/img/portadas/Frozen II.jpeg",
-        "/resources/img/portadas/Gladiador.jpg",
-        "/resources/img/portadas/Gran Torino.jpg",
-        "/resources/img/portadas/Indiana Jones En busca del arca perdida.png",
-        "/resources/img/portadas/Jurassic Park.jpg",
-        "/resources/img/portadas/La Ballena.jpeg",
-        "/resources/img/portadas/La Guerra de las Galaxias.jpg",
-        "/resources/img/portadas/Los Infiltrados.jpg",
-        "/resources/img/portadas/Luca.jpeg",
-        "/resources/img/portadas/Matrix.jpg",
-        "/resources/img/portadas/Minions El Origen de Gru.jpg",
-        "/resources/img/portadas/MisionImposible Sentencia Mortal - Parte uno.jpg",
-        "/resources/img/portadas/Oppenheimer.jpg",
-        "/resources/img/portadas/Slumdog Millionaire.jpg",
-        "/resources/img/portadas/Soul.jpg",
-        "/resources/img/portadas/Spider Man Cruzando el multiverso.jpg",
-        "/resources/img/portadas/Tiempos Modernos.jpg",
-        "/resources/img/portadas/Titanic.jpg",
-        "/resources/img/portadas/Toy Story 4.jpg",
-        "/resources/img/portadas/Volver al Futuro.jpg"
+        "resources/img/portadas/12 Años de Esclavitud.jpg",
+        "resources/img/portadas/Alien El Octavo Pasajero.jpg",
+        "resources/img/portadas/Barbie.jpg",
+        "resources/img/portadas/Blade Runner.jpg",
+        "resources/img/portadas/Casablanca.jpg",
+        "resources/img/portadas/Dune Parte uno.jpg",
+        "resources/img/portadas/El Caballero Oscuro.jpg",
+        "resources/img/portadas/El Club de la Pelea.jpg",
+        "resources/img/portadas/El Gran Escape.jpg",
+        "resources/img/portadas/El Padrino.jpg",
+        "resources/img/portadas/El renacido.jpg",
+        "resources/img/portadas/El Rey Leon.jpg",
+        "resources/img/portadas/El Señor de los Anillos La Comunidad del Anillo.jpg",
+        "resources/img/portadas/Encanto.jpeg",
+        "resources/img/portadas/Forrest Gump.jpg",
+        "resources/img/portadas/Frozen II.jpeg",
+        "resources/img/portadas/Gladiador.jpg",
+        "resources/img/portadas/Gran Torino.jpg",
+        "resources/img/portadas/Indiana Jones En busca del arca perdida.png",
+        "resources/img/portadas/Jurassic Park.jpg",
+        "resources/img/portadas/La Ballena.jpeg",
+        "resources/img/portadas/La Guerra de las Galaxias.jpg",
+        "resources/img/portadas/Los Infiltrados.jpg",
+        "resources/img/portadas/Luca.jpeg",
+        "resources/img/portadas/Matrix.jpg",
+        "resources/img/portadas/Minions El Origen de Gru.jpg",
+        "resources/img/portadas/MisionImposible Sentencia Mortal - Parte uno.jpg",
+        "resources/img/portadas/Oppenheimer.jpg",
+        "resources/img/portadas/Slumdog Millionaire.jpg",
+        "resources/img/portadas/Soul.jpg",
+        "resources/img/portadas/Spider Man Cruzando el multiverso.jpg",
+        "resources/img/portadas/Tiempos Modernos.jpg",
+        "resources/img/portadas/Titanic.jpg",
+        "resources/img/portadas/Toy Story 4.jpg",
+        "resources/img/portadas/Volver al Futuro.jpg"
     );
 
     private List<String> titulosPeliculas = List.of(
@@ -116,11 +116,15 @@ public class VentanaInicial extends JFrame {
                 // Usar una imagen de reemplazo o continuar sin imagen
                 imagenIcono = new ImageIcon(new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB));
             }
-
+            
             // Escalar la imagen
             ImageIcon imagenEscalada = new ImageIcon(
                 imagenIcono.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-            JLabel imagenPelicula = new JLabel(imagenEscalada, JLabel.CENTER);
+           
+            
+            //ROBERTO ES AQUI
+            JLabel imagenPelicula = new JLabel("", JLabel.CENTER);
+            imagenPelicula.setIcon(imagenEscalada);
             imagenPelicula.setPreferredSize(new Dimension(100, 100));
 
             // Título de la película
